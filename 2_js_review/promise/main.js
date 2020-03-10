a5a520c747ca364827ee4ab9c7c43dec57db56dd
Zsolt Toth <tothzs87@gmail.com>
2020-03-10 09:18:51 +0100

    promise examples
    promose is demonstrated

--- 2_js_review/promise/main.js
+++ 2_js_review/promise/main.js
@@ -1,4 +1,8 @@
-var promise = new Promise((resolve, reject)=>{
+var promise = new Promise((resolve)=>{
+    console.log("In the promise");
     resolve();
 });
+
+console.log("Hello");
 promise.then(()=>{console.log("promise is done")});
+console.log("Bye bye");
