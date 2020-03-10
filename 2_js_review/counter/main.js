f797b72a33c3abdb44a08303728d5977c006926d
Zsolt Toth <tothzs87@gmail.com>
2020-03-10 08:30:56 +0100

    counter example
    Find another problem with the counter. You cannot use two counters at the same time.

--- 2_js_review/counter/main.js
+++ 2_js_review/counter/main.js
@@ -1,11 +1,14 @@
 /*
 Lets create a simple counter which can be increased or reset.
  */
-
 const counter = require('./counter');
+const counter2 = require('./counter');
 
 
 console.log(counter.increase());
 console.log(counter.increase());
 console.log(counter.increase());
 
+console.log(counter2.increase());
+console.log(counter2.increase());
+console.log(counter2.increase());
