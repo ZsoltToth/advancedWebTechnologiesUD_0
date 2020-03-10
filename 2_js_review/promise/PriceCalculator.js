c02f51e1c3d46b05d1fce83d3606926e254180b6
Zsolt Toth <tothzs87@gmail.com>
2020-03-10 09:31:27 +0100

    promise examples
    Product Price Callculator blocking implementation of total price calculation is added.

--- 2_js_review/promise/PriceCalculator.js
+++ 2_js_review/promise/PriceCalculator.js
@@ -4,7 +4,13 @@ class PriceCalculator{
     }
 
     totalGrossPrice_BlockingImpl(products,callback){
-        callback("Implement Me!");
+        callback(products.reduce((total, current)=>{
+            if(!current.isTaxable){
+                return total + current.netPrice;
+
+            }
+            return total + current.netPrice * (100.0 + this.vatRate) / 100.0;
+        },0));
     }
 }
 
