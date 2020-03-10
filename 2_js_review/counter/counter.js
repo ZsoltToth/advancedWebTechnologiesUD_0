42775c3ad6e228c07862b8fc2bf135416706a3fa
Zsolt Toth <tothzs87@gmail.com>
2020-03-10 08:28:38 +0100

    counter example
    counter was converted to a closure.

--- 2_js_review/counter/counter.js
+++ 2_js_review/counter/counter.js
@@ -1,10 +1,8 @@
-var count = 0;
-var increase = function () {
-    count = count + 1;
-    console.log("increment to "+count);
-}
+var increase = (function () {
+    var count = 0;
+    return function(){ count++; return count;};
+})();
 
 module.exports  = {
-    count : count,
     increase : increase
 };
--- 2_js_review/counter/main.js
+++ 2_js_review/counter/main.js
@@ -4,9 +4,8 @@ Lets create a simple counter which can be increased or reset.
 
 const counter = require('./counter');
 
-console.log(counter.count);
-counter.increase()
-console.log(counter.count);
-counter.increase()
-console.log(counter.count);
-counter.increase()
+
+console.log(counter.increase());
+console.log(counter.increase());
+console.log(counter.increase());
+
