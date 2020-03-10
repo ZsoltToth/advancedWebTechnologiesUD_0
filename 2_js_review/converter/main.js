393a673821b69c50165d8ecc543b3172d9809d76
Zsolt Toth <tothzs87@gmail.com>
2020-03-10 08:58:46 +0100

    converter example for callbacks
    Custom callback implementation was added to the main script of the converter example.

--- 2_js_review/converter/main.js
+++ 2_js_review/converter/main.js
@@ -1,3 +1,12 @@
 const temp_converter = require('./temperature');
 
 temp_converter.celsius2farenheit(0, console.log);
+
+temp_converter.farenheit2celsius(32, (celsius)=>{
+   if(celsius < 10){
+       console.log("Too cold");
+   }
+   else{
+       console.log("Its OK");
+   }
+});
