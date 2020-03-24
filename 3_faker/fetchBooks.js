d51933ecfd360751f519b61e337b055b5d488716
Zsolt Toth <tothzs87@gmail.com>
2020-03-24 09:16:37 +0100

    axios is fixed.
    get function call expects full URL.

--- 3_faker/fetchBooks.js
+++ 3_faker/fetchBooks.js
@@ -1,5 +1,5 @@
 var axios = require('axios');
 
-axios.get('127.0.0.1:3001/authors?authorId=97')
+axios.get('http://localhost:3001/authors?authorId=97')
     .then((response)=>{console.log(response.data)})
     .catch((err)=>{console.log('problem\n'+err)});
