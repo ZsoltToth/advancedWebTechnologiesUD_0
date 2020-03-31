8e51374a2802571a5909c46bf23f1946051f1e3b
Zsolt Toth <tothzs87@gmail.com>
2020-03-31 09:01:36 +0200

    Bootstrap was compiled into our react application.

--- 4_react/src/App.js
+++ 4_react/src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+//import './App.css';
+import './style.scss';
 import './components/Greetings';
 import Greetings from "./components/Greetings";
 import NewsFeed from "./components/NewsFeed";
 
 function App() {
   return (
-    <div className="App">
+    <div className={"App","container-fluid"}>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
           <Greetings name={"Alice"}/>
--- 4_react/src/components/NewsItem.js
+++ 4_react/src/components/NewsItem.js
@@ -1,6 +1,6 @@
 import React from "react";
 
 function NewsItem(props) {
-    return <div><h1>{props.title}</h1><p>{props.content}</p></div>
+    return <div class="card"><h1 class="card-title">{props.title}</h1><p class="card-body">{props.content}</p></div>
 }
 export default NewsItem
