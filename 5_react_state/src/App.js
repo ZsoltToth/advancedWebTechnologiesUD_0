5ed1e8bbdc867a31cec160a9119a0cc833eecf03
Zsolt Toth <tothzs87@gmail.com>
2020-04-07 09:26:46 +0200

    Form is handled and Table is populated.

--- 5_react_state/src/App.js
+++ 5_react_state/src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.scss';
+import ToDoList from "./components/ToDoList";
 
 function App() {
   return (
     <div className="container-fluid">
       <div className="row">
           <div className="col-md-2"></div>
-          <div className="col-md-8"></div>
+          <div className="col-md-8"><ToDoList/></div>
           <div className="col-md-2"></div>
       </div>
     </div>
--- 5_react_state/src/components/ToDoList.js
+++ 5_react_state/src/components/ToDoList.js
@@ -2,11 +2,130 @@ import React from 'react'
 
 class ToDoList extends React.Component{
 
+
+    constructor(props){
+        super(props);
+        this.state = {
+            form : {
+                name: "Dish Washing",
+                description: "I shall wash the dishes",
+                estimation: "0.5"
+            },
+            todos : []
+        };
+        this.handleNameChangeEvent = this.handleNameChangeEvent.bind(this);
+        this.handleDescriptionChangeEvent = this.handleDescriptionChangeEvent.bind(this);
+        this.handleEstimationChangeEvent = this.handleEstimationChangeEvent.bind(this);
+        this.handleAddOnClickEvent = this.handleAddOnClickEvent.bind(this)
+    }
+
+    handleNameChangeEvent(event){
+        let state = this.state;
+        state.form.name = event.target.value;
+        this.setState(state);
+    }
+
+    handleDescriptionChangeEvent(event){
+        let state = this.state;
+        state.form.description = event.target.value;
+        this.setState(state);
+    }
+
+    handleEstimationChangeEvent(event){
+        let state = this.state;
+        state.form.estimation = event.target.value;
+        this.setState(state);
+    }
+
+    handleAddOnClickEvent(event){
+        let state = this.state;
+        state.todos.push(this.state.form);
+        state.form = {name : '', description: '', estimation: 0};
+        this.setState(state);
+
+    }
+
     render(){
         return (
-            <div className="row">
-                <h1>To Do List</h1>
+            <div>
+                <div className="row">
+                    <h1>To Do List</h1>
 
+                </div>
+                <div className="row">
+                    <table>
+                        <tbody>
+                        <tr>
+                            <td>
+                                To Do Name:
+                            </td>
+                            <td>
+                                <input
+                                    type="text"
+                                    value={this.state.form.name}
+                                    onChange={this.handleNameChangeEvent}
+                                />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                Description:
+                            </td>
+                            <td>
+                                <input
+                                    type="text"
+                                    value={this.state.form.description}
+                                    onChange={this.handleDescriptionChangeEvent}
+                                />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                Estimated Time:
+                            </td>
+                            <td>
+                                <input
+                                    type="number"
+                                    value={this.state.form.estimation}
+                                    onChange={this.handleEstimationChangeEvent}
+                                />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td colSpan={2}>
+                                <button onClick={this.handleAddOnClickEvent}>Add</button>
+                            </td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div className="row">-----------</div>
+                <div className="row">
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Estimation</th>
+                                <th>Description</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        {
+                            this.state.todos.map((todoItem)=>{
+                                return(
+                                    <tr key={todoItem.name}>
+                                        <td>{todoItem.name}</td>
+                                        <td>{todoItem.estimation}</td>
+                                        <td>{todoItem.description}</td>
+                                        <td><button className="btn-danger">Delete</button></td>
+                                    </tr>
+                                )
+                            })
+                        }
+                        </tbody>
+                    </table>
+                </div>
             </div>
         );
     }
