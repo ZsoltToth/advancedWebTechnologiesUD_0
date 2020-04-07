9db9e25b8b826e5e8ff4145dfbc6c5eabeee223a
Zsolt Toth <tothzs87@gmail.com>
2020-04-07 08:21:45 +0200

    ToDoList.js component was created

--- 5_react_state/src/App.js
+++ 5_react_state/src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-fluid">
+      <div className="row">
+          <div className="col-md-2"></div>
+          <div className="col-md-8"></div>
+          <div className="col-md-2"></div>
+      </div>
     </div>
   );
 }
---
+++ 5_react_state/src/components/ToDoList.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class ToDoList extends React.Component{
+
+    render(){
+        return (
+            <div className="row">
+                <h1>To Do List</h1>
+
+            </div>
+        );
+    }
+}
+
+export default ToDoList
