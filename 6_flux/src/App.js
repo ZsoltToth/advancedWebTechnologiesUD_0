073c42efdf886cadbeb42b27ae8fd3d85247091a
Zsolt Toth <tothzs87@gmail.com>
2020-04-14 09:16:16 +0200

    Button is added to a common lottery type

--- 6_flux/src/App.js
+++ 6_flux/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
 import LotteryGeneratorForm from "./components/LotteryGeneratorForm";
+import LotteryActions from "./actions/LotteryActions";
 
 function App() {
   return (
     <div className="App">
+        <button onClick={()=>{
+            LotteryActions.generatePicks(1,90,5)
+        }}>Lottery 5</button>
         <LotteryGeneratorForm/>
         <div id={"picks"}></div>
     </div>
