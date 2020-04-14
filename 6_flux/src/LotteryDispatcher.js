f6a4475516956eb0f0d3c2dcb2114aa1427f32fc
Zsolt Toth <tothzs87@gmail.com>
2020-04-14 09:11:17 +0200

    Dispatcher handles action

--- 6_flux/src/LotteryDispatcher.js
+++ 6_flux/src/LotteryDispatcher.js
@@ -1,4 +1,7 @@
 import {Dispatcher} from 'flux'
+import ReactDOM from "react-dom";
+import LotteryPicks from "./components/LotteryPicks";
+import React from "react";
 
 class LotteryDispatcher extends Dispatcher{
 
@@ -11,5 +14,34 @@ class LotteryDispatcher extends Dispatcher{
 }
 
 const dispatcher = new LotteryDispatcher();
+
+dispatcher.register((payload)=>{
+
+    if(payload.action.actionType !== 'LOTTERY_PICK_GENERATION'){
+        return;
+    }
+    let picks = [];
+    let min = payload.action.payload.min;
+    let max = payload.action.payload.max;
+    let pickCnt = payload.action.payload.pickCnt;
+
+    for(let i = 0; i < pickCnt; i++){
+        let currentPick = Math.round(
+            Math.random() *
+            (max - min)
+        ) + min;
+        if(picks.includes(currentPick)){
+            i--;
+        }
+        else{
+            picks.push(currentPick);
+        }
+    }
+    ReactDOM.render(
+        <LotteryPicks picks={picks}/>,
+        document.getElementById('picks'));
+
+});
+
 
 export default  dispatcher;
--- 6_flux/src/actions/LotteryActions.js
+++ 6_flux/src/actions/LotteryActions.js
@@ -1,27 +1,16 @@
 import dispatcher from '../LotteryDispatcher'
-import ReactDOM from "react-dom";
-import LotteryPicks from "../components/LotteryPicks";
-import React from "react";
 
 class LotteryActions{
 
     generatePicks(min, max, pickCnt){
-        let picks = [];
-        for( let i = 0; i < pickCnt; i++){
-            let currentPick = Math.round(
-                Math.random() *
-                (max - min)
-            ) + min;
-            if(picks.includes(currentPick)){
-                i--;
+        dispatcher.handleViewAction({
+            actionType : 'LOTTERY_PICK_GENERATION',
+            payload : {
+                min : min,
+                max : max,
+                pickCnt : pickCnt,
             }
-            else{
-                picks.push(currentPick);
-            }
-        }
-        ReactDOM.render(
-            <LotteryPicks picks={picks}/>,
-            document.getElementById('picks'));
+        });
     }
 
 }
